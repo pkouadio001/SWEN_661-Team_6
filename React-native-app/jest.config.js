d3db3e6c8ee54f1836c3fd2c2bb4198155151e06
pkouadio001 <150391851+pkouadio001@users.noreply.github.com>
2026-02-17 23:44:11 -0500

    Delete React-native-app directory

--- React-native-app/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "jest-expo",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testEnvironment: "node",
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
-  },
-};
